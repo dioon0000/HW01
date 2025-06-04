6fc959a9acf183186f39e811fb5d48d6a71bbe0f
DiooN <dioon0000@naver.com>
2025-06-04 19:45:29 +0900

    도전 기능 구현

--- HW01/HW01.cpp
+++ HW01/HW01.cpp
@@ -15,14 +15,54 @@ int avgFunc(vector<int> v){
 
     return val;
 }
-void sortFunc(vector<int>& v, int c){
-    //c가 1이면 오름차순 정렬
-    //c가 2면 내림차순 정렬
+
+//퀵정렬 함수
+void quickSort(vector<int>& v, int start, int end, int command) {
+    if(start >= end) return;
+    int pivot = start;
+    int i = start + 1;
+    int j = end;
+
+    while(i <= j){
+        if(command == 1){
+            while(i <= end && v[i] <= v[pivot]) i++;
+            while(j > start && v[j] >= v[pivot]) j--;
+        } 
+        else {
+            while(i <= end && v[i] >= v[pivot]) i++;
+            while(j > start && v[j] <= v[pivot]) j--;
+        }
+        if(i > j) swap(v[pivot], v[j]);
+        else swap(v[i], v[j]);
+    }
+    quickSort(v, start, j - 1, command);
+    quickSort(v, j + 1, end, command);
 }
+
+void sortFunc(vector<int>& v, int command){
+
+    //c가 1이면 오름차순 정렬,c가 2면 내림차순 정렬
+    if(command == 1 || command == 2){
+        quickSort(v, 0, v.size()-1, command);
+        cout << "정렬된 배열 : ";
+        for(int i=0; i<v.size(); ++i)
+            cout << v[i] << ' ';
+        if(command == 1)
+            cout << "\n오름차순으로 정렬되었습니다";
+        else
+            cout << "\n내림차순으로 정렬되었습니다";
+    }
+    //그 외 커멘드 입력
+    else{
+        cout << "잘못된 정렬 커멘드 입력입니다";
+        return ;
+    }
+}
+
 int main(){
     //변수 선언
     vector<int> v;
-    int x, n, add=0, avg=0;
+    int x, n, c, add=0, avg=0;
     //입력
     cout << "벡터의 크기 N값을 입력해주세요: ";
     cin >> n;
@@ -33,11 +73,16 @@ int main(){
         v.push_back(x);
     }
     avg+=add/v.size();
+    cout << "정렬하고 싶은 방식을 입력해주세요 (1: 오름차순, 2: 내림차순): ";
+    cin >> c;
 
     //출력
+    cout << "필수과제 구현\n=================================================\n";
     cout << "배열에 저장된 숫자들의 합계: " << add << '\n';
     cout << "배열에 저장된 숫자들의 평균: " << avg << '\n';
     cout << "배열에 저장된 숫자들의 합계(함수호출): " << addFunc(v) << '\n';
     cout << "배열에 저장된 숫자들의 평균(함수호출): " << avgFunc(v) << '\n';
+    cout << "=================================================\n도전과제 구현\n";
+    sortFunc(v, c);
     return 0;
 }
